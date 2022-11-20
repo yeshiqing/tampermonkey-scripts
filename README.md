ae85fdd5997f87557147c7f570bcb6ba8a73b155
jeffrey <jeffreyye@qq.com>
2022-11-20 21:29:13 +0800

    v0.0.6

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # 油猴脚本
-这些是个人的油猴用户脚本，可根据意愿改动。
+这些是个人的油猴用户脚本，大家安装后可根据意愿自由改动。
 
 ## 脚本安装
 1. 首先在 Chrome 中安装用户脚本管理器 [Tampermonkey 插件](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)
@@ -7,7 +7,7 @@
 3. 如果仍有问题可查看[用户脚本安装手册](https://greasyfork.org/zh-CN/help/installing-user-scripts?locale_override=1)
 
 ## 脚本名称列表
-- [jirengu video hotkeys](https://greasyfork.org/en/scripts/454346-jirengu-video-hotkeys)
+- [jirengu video hotkeys](https://greasyfork.org/en/scripts/454346-jirengu-video-hotkeys)：饥人谷课程视频快捷键
 
 ## 脚本自定义
 1）脚本注释中的元键  
--- jirengu-video-hotkeys.js
+++ jirengu-video-hotkeys.js
@@ -7,20 +7,28 @@
 // @match          https://xiedaimala.com/tasks/*
 // @match          https://jirengu.com/tasks/*
 // @grant          none
-// @version        0.0.5
+// @version        0.0.6
 // @namespace      https://github.com/yeshiqing/tampermonkey-scripts
 // @icon           data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // ==/UserScript==
 
-const VIDEO_AUTOPLAY = true
-const VOLUME_ADJUST_ARROWKEY_DOWN = 3
+/**
+ * *_DISABLE 表示禁用原有事件
+ * *_HIJACK 表示劫持原事件，插入 hook
+ */
 const IGNOREEVENTS = ['mousemove', 'mouseenter', 'mouseleave', 'mouseover'] // 用于研究事件劫持 // ['mousedown']
-const DEBUG_MODE = true
-const LOG_VIDEO_STATUS = false
-const VIDEO_DBLCLICK_DISABLE = true        // disable double click to fullscreen. I don't use fullscreen and it may unintentionally be triggered when toggle application.
-const ESCAPE_KEYUP_HIJACK = true           // escape keyup 加入 hook
-const VIDEO_CLICK_DISABLE = false          // 禁用单击视频事件
 const TRIGGER_WEBPACKJSONP_API = true      // 调用饥人谷提供的 webpackJsonp API
+const VOLUME_ADJUST_ARROWKEY_DOWN = 3      // 左右方向键调整视频快进快退的粒度
+const ESCAPE_KEYUP_HIJACK = true           // escape keyup 加入 hook
+const VIDEO_DBLCLICK_DISABLE = true        // 禁用视频原有的双击事件，即取消切换「全屏」。I don't use fullscreen and it may unintentionally be triggered when toggle application.
+const VIDEO_CLICK_DISABLE = false          // 用于调试，不触发源代码中的函数。按快捷键前需要点击一下聚焦 video。
+const CMD_KEYDOWN_DISABLE = false
+const F_KEYUP_HIJACK = true
+const F_KEYUP_DISABLE = true
+const AUTO_FULLWINDOW = false
+const LOG_VIDEO_STATUS = false
+const VIDEO_AUTOPLAY = true
+const DEBUG_EVENT = false
 
 const EVENTS_CONFIG = {
     'keyup': [{
@@ -30,20 +38,28 @@ const EVENTS_CONFIG = {
         'fn': 'pauseVideo',
         'hijack': ESCAPE_KEYUP_HIJACK, // 是否插入 hook
         'disable': false         // 是否禁用原有事件处理程序
+    }, {
+            'eventName': 'keyup',
+            'key': 'f',
+            'this': document,
+            'fn': 'websiteScreen',
+            'hijack': F_KEYUP_HIJACK,
+            'disable': F_KEYUP_DISABLE
+        }],
+    'keydown': [{
+        'eventName': 'keydown',
+        'key': 'Meta',
+        'this': document,
+        'disable': CMD_KEYDOWN_DISABLE
     }],
     'click': [{
         'eventName': 'click',
-        'key': null,
-        'this': video,
-        'fn': 'click-video',
-        'hijack': true,
+        'this': 'video',
         'disable': VIDEO_CLICK_DISABLE
     }],
     'dblclick': [{
         'eventName': 'dblclick',
-        'key': null,
         'this': 'video',
-        'fn': 'video-dblclick-disable',
         'disable': VIDEO_DBLCLICK_DISABLE
     }]
 }
@@ -57,7 +73,7 @@ window.onload = () => {
         if (VIDEO_AUTOPLAY) {
             let ele_playButton = document.querySelector('.vjs-big-play-button')
             ele_playButton && ele_playButton.click()
-            if (!DEBUG_MODE) {
+            if (AUTO_FULLWINDOW) {
                 let ele_fullWin = document.querySelector('.video-js-fullwindow-button')
                 ele_fullWin && ele_fullWin.click()
             }
@@ -71,15 +87,13 @@ window.addEventListener('keydown', (e) => {
     let nodeList = document.querySelectorAll('.task-name')
     let length = nodeList.length
     let span_nextTask = nodeList[length - 1]
-    if (span_nextTask) {
-        if (e.ctrlKey && e.altKey && e.key === 'ArrowRight') {
-            span_nextTask.click()
-        }
+    if (span_nextTask && e.ctrlKey && e.altKey && e.key === 'ArrowRight') {
+        span_nextTask.click()
     }
 })
 
 var video = document.querySelector('video')
-let video_status = 'loadedmetadata' // loadedmetadata playing play waiting pause ended
+let video_status = 'loadedmetadata' // loadedmetadata playing play waiting pause ended 
 let $hijack = {
     events: EVENTS_CONFIG,
     videoEvents: {
@@ -90,6 +104,10 @@ let $hijack = {
         'hijack': true,
         'disable': false
     },
+    // 切换「网页全屏」
+    'websiteScreen'(event) {
+        document.dispatchEvent(new CustomEvent("videoToggleFullWindow"))
+    },
     'pauseVideo'(event) {
         // 停止播放
         setTimeout(() => { // 应对异步操作：UI 更新
@@ -98,8 +116,6 @@ let $hijack = {
                 video.click()
             }
         }, 0)
-    },
-    'click-video'(event) {
     },
     'setVideoStatus'(event) {
         video_status = event.type
@@ -113,9 +129,9 @@ let $hijack = {
         if (obj) {
             let { disable, hijack } = obj
             disable = (typeof disable === 'boolean' ? disable : false) // 默认 disable 为 false
-            hijack = (typeof hijack === 'boolean' ? hijack : true) // 默认 hijack 为 true
-            let fn = $hijack[obj.fn] || function (e) { };
-            hijack && fn(event)
+            hijack = (typeof hijack === 'boolean' ? hijack : false) // 默认 hijack 为 false
+
+            hijack && ($hijack[obj.fn] || function (e) { })(event)
 
             return disable
         }
@@ -127,7 +143,8 @@ let $hijack = {
         } else if (typeof selector === 'string') {
             return document.querySelector(selector)
         }
-        return null
+        console.error("can't find the specified element")
+        return window
     },
     getEvent(event) {
         let eventName = event.type
@@ -142,14 +159,16 @@ let $hijack = {
             let currentTarget = event.currentTarget
             let key = event.key
             let obj = arr.find((el, i) => {
-                return $hijack.getThis(el['this']) === currentTarget && (el[key] ? el[key] === key : true)
+                return $hijack.getThis(el['this']) === currentTarget && (el.key ? el.key === key : true)
             })
             return obj || null
         }
         return null
     }
 }
-
+const GLOBAL = {
+    Ft: null // Ft 对象位于 show-pc 文件 2948 行，它有很多方法，比如控制 video 的 Ft.play() Ft.pause() Ft.exitFullscreen() Ft.requestFullscreen()
+}
 const OLD_ADD_EL = EventTarget.prototype.addEventListener
 EventTarget.prototype.addEventListener = function (eventName, fn, ...args) {
     if (IGNOREEVENTS.includes(eventName)) {
@@ -163,6 +182,11 @@ EventTarget.prototype.addEventListener = function (eventName, fn, ...args) {
     }
 
     OLD_ADD_EL.call(this, eventName, function foo(event) {
+
+        if (DEBUG_EVENT && event.type === 'click' && event.currentTarget === video) {
+            // debugger
+        }
+
         if ($hijack.trigger(event)) {
             return
         }
@@ -175,8 +199,14 @@ EventTarget.prototype.addEventListener = function (eventName, fn, ...args) {
 {
     const DIGIT_RANDOM = Date.now();
     const ID3145 = Number('' + 3145 + DIGIT_RANDOM) // override ArrowRight & ArrowLeft hotkeys
-    const start_arr = TRIGGER_WEBPACKJSONP_API ? [ID3145] : []
+    const start_arr = TRIGGER_WEBPACKJSONP_API ? [ID3145, DIGIT_RANDOM] : []
+    let exportGlobal = function (Ft) {
+        GLOBAL.Ft = GLOBAL.Ft || Ft // 利用 webpackJsonp API 导出 Ft
+    }
     window.webpackJsonp([], {
+        [DIGIT_RANDOM]: function () {
+            console.log(arguments);
+        },
         [ID3145]: function (e, t, n) {
             "use strict";
             var i = n(114)
@@ -186,8 +216,9 @@ EventTarget.prototype.addEventListener = function (eventName, fn, ...args) {
                 , o = n(620)
                 , u = {
                     " ": {
-                        pressUp: function (e) {
-                            e.paused() ? e.play() : e.pause()
+                        pressUp: function (Ft) {
+                            exportGlobal(Ft)
+                            Ft.paused() ? Ft.play() : Ft.pause()
                         },
                         longPressStart: function (e) {
                             e.paused() || o.a.explode({
@@ -198,19 +229,23 @@ EventTarget.prototype.addEventListener = function (eventName, fn, ...args) {
                             e.paused() || o.a.resume()
                         }
                     },
+                    // 进入/退出「全屏」模式
                     f: {
-                        pressUp: function (e) {
-                            return e.isFullscreen() ? e.exitFullscreen() : e.requestFullscreen()
+                        pressUp: function (Ft) {
+                            exportGlobal(Ft)
+                            return Ft.isFullscreen() ? Ft.exitFullscreen() : Ft.requestFullscreen()
                         }
                     },
                     ArrowLeft: {
-                        pressDown: function (e) {
-                            return e.currentTime(e.currentTime() - VOLUME_ADJUST_ARROWKEY_DOWN)
+                        pressDown: function (Ft) {
+                            exportGlobal(Ft)
+                            return Ft.currentTime(Ft.currentTime() - VOLUME_ADJUST_ARROWKEY_DOWN)
                         }
                     },
                     ArrowRight: {
-                        pressDown: function (e) {
-                            return e.currentTime(e.currentTime() + VOLUME_ADJUST_ARROWKEY_DOWN)
+                        pressDown: function (Ft) {
+                            exportGlobal(Ft)
+                            return Ft.currentTime(Ft.currentTime() + VOLUME_ADJUST_ARROWKEY_DOWN)
                         }
                     },
                     ArrowDown: {
@@ -224,12 +259,14 @@ EventTarget.prototype.addEventListener = function (eventName, fn, ...args) {
                         }
                     },
                     Escape: {
-                        pressUp: function (e) {
+                        pressUp: function (Ft) {
+                            exportGlobal(Ft)
                             return document.dispatchEvent(new CustomEvent("videoExitFullWindow"))
                         }
                     },
+                    // 进入/退出「网页全屏」模式
                     w: {
-                        pressUp: function (e) {
+                        pressUp: function (Ft) {
                             return document.dispatchEvent(new CustomEvent("videoToggleFullWindow"))
                         }
                     }
