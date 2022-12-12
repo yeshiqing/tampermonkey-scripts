1a9203fde44508e3631c3e54bcc3169709f5dd8b
jeffrey <jeffreyye@qq.com>
2022-12-12 10:20:10 +0800

    v0.0.4 use `Object.defineProperty()` to fix bug

--- scripts/chrome-devtools-console.js
+++ scripts/chrome-devtools-console.js
@@ -6,40 +6,36 @@
 // @run-at         document-start
 // @match          *://*/*
 // @grant          none
-// @version        0.0.3
+// @version        0.0.4
 // @namespace      https://github.com/yeshiqing/tampermonkey-scripts
 // @icon           https://upload-images.jianshu.io/upload_images/1231311-26b5e3552753c5bb.png
 // ==/UserScript==
 
 let isFn = function (x) { return (x instanceof Function) }
 
+let defineProperty = (obj, prop, getFn) => {
+    let value
+    Object.defineProperty(obj, prop, {
+        "configurable": true,
+        "enumerable": false,
+        get() { return value ?? getFn },
+        set(v) { value = v }
+    })
+}
+
 let generate_console = function (...props) {
     props.forEach((prop, i) => {
-        let cover = null
-        Object.defineProperty(window, prop, {
-            "configurable": true,
-            "enumerable": false,
-            get() {
-                return function (...args) {
-                    cover && isFn(cover) && cover()
-                    console[prop](...args)
-                }
-            },
-            set(v) {
-                console.warn(`试图对 window.${prop} 进行赋值`)
-                cover = v
-            }
+        defineProperty(window, prop, function (...args) {
+            console[prop](...args)
         })
     })
 }
 
 let extend_Object_prototype = function (...props) {
     props.forEach((prop, i) => {
-        if (Object.prototype[prop] == null) {
-            Object.prototype[prop] = function () {
-                console[prop](this)
-            }
-        }
+        defineProperty(Object.prototype, prop, function () {
+            console[prop](this)
+        })
     })
 }
 
