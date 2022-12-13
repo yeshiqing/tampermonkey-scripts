0bd8ad564877c6fadfc6105639c313260055286f
jeffrey <jeffreyye@qq.com>
2022-12-13 20:58:25 +0800

    v0.0.5 应对“掘金”主页，对象存在重名键 dir

--- scripts/chrome-devtools-console.js
+++ scripts/chrome-devtools-console.js
@@ -3,10 +3,10 @@
 // @description    "console.log()" and "console.dir()" faster in the DevTools of Chrome
 // @author         yeshiqing
 // @license        MIT
-// @run-at         document-start
+// @run-at         document-idle
 // @match          *://*/*
 // @grant          none
-// @version        0.0.4
+// @version        0.0.5
 // @namespace      https://github.com/yeshiqing/tampermonkey-scripts
 // @icon           https://upload-images.jianshu.io/upload_images/1231311-26b5e3552753c5bb.png
 // ==/UserScript==
@@ -25,20 +25,25 @@ let defineProperty = (obj, prop, getFn) => {
 
 let generate_console = function (...props) {
     props.forEach((prop, i) => {
-        defineProperty(window, prop, function (...args) {
-            console[prop](...args)
-        })
+        (window.prop == undefined) &&
+            defineProperty(window, prop, function (...args) {
+                console[prop](...args)
+            })
     })
 }
 
 let extend_Object_prototype = function (...props) {
     props.forEach((prop, i) => {
-        defineProperty(Object.prototype, prop, function () {
-            console[prop](this)
-        })
+        !(prop in Object.prototype) &&
+            defineProperty(Object.prototype, prop, function () {
+                console[prop](this)
+            })
     })
 }
 
+
 generate_console('log', 'dir')
 
-extend_Object_prototype('log', 'dir')
+setTimeout(() => { // 应对“掘金”主页，对象存在重名键 dir
+    extend_Object_prototype('log', 'dir')
+}, 1000)
