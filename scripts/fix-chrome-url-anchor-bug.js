f49c1e33a7de564492741ae5d3beb75028d4e338
jeffrey <jeffreyye@qq.com>
2022-12-28 18:09:32 +0800

    v0.0.3 应对 Vue 官方说明文档异步加载

--- scripts/fix-chrome-url-anchor-bug.js
+++ scripts/fix-chrome-url-anchor-bug.js
@@ -5,8 +5,9 @@
 // @license        MIT
 // @run-at         document-idle
 // @match          https://developer.mozilla.org/*
+// @match          https://*.vuejs.org/*
 // @grant          none
-// @version        0.0.2
+// @version        0.0.3
 // @namespace      https://github.com/yeshiqing/tampermonkey-scripts
 // @icon           https://upload-images.jianshu.io/upload_images/1231311-26b5e3552753c5bb.png
 // ==/UserScript==
@@ -17,7 +18,10 @@ if (isChrome) {
     let old_hash = location.hash
     if (old_hash !== '') {
         window.addEventListener('load', (e) => { // DOM节点加载完毕，页面图片和样式都已加载完毕
-            window.location = old_hash
+            setTimeout(() => {
+                window.location = old_hash
+            }, 100) // 应对文档异步加载
         })
+
     }
 }
