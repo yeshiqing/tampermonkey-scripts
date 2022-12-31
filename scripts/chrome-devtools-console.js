486165a24f643b210edcedbf1524c58edea59535
jeffrey <jeffreyye@qq.com>
2022-12-31 21:40:00 +0800

    v0.0.7 受 Vue 源码启发，Object.prototype.log 的函数名为 log

--- scripts/chrome-devtools-console.js
+++ scripts/chrome-devtools-console.js
@@ -6,7 +6,7 @@
 // @run-at         document-idle
 // @match          *://*/*
 // @grant          none
-// @version        0.0.6
+// @version        0.0.7
 // @namespace      https://github.com/yeshiqing/tampermonkey-scripts
 // @icon           https://upload-images.jianshu.io/upload_images/1231311-26b5e3552753c5bb.png
 // ==/UserScript==
@@ -31,25 +31,18 @@ let generate_console = function (...props) {
     })
 }
 
-let extend_Object_prototype = function (...props) {
-    props.forEach((prop, i) => {
-        let getFn = function () {
-            console[prop](this)
-        }
-        Object.defineProperty(getFn, 'name', {
-            "configurable": true,
-            "enumerable": false,
-            "writable": false,
-            "value": prop
-        })
-        !(prop in Object.prototype) &&
-            defineProperty(Object.prototype, prop, getFn)
+let setEnumerableFalse = function (obj, prop) {
+    Object.defineProperty(obj, prop, {
+        "enumerable": false,
     })
 }
 
-
 generate_console('log', 'dir')
 
+
+!('log' in Object.prototype) && (Object.prototype.log = function log() { console.log(this) })
+setEnumerableFalse(Object.prototype, 'log')
 setTimeout(() => { // 应对“掘金”主页，对象存在重名键 dir
-    extend_Object_prototype('log', 'dir')
+    !('dir' in Object.prototype) && (Object.prototype.dir = function dir() { console.dir(this) })
+    setEnumerableFalse(Object.prototype, 'dir')
 }, 1000)
