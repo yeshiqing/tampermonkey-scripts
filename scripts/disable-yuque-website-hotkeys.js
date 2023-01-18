00e2b61a9bef8562524384b0eb345ef27dbeebf4
jeffrey <jeffreyye@qq.com>
2023-01-18 20:58:06 +0800

    modify deprecated API

--- scripts/disable-yuque-website-hotkeys.js
+++ scripts/disable-yuque-website-hotkeys.js
@@ -6,43 +6,40 @@
 // @run-at         document-start
 // @match          https://www.yuque.com/*
 // @grant          none
-// @version        0.0.6
+// @version        0.0.7
 // @namespace      https://github.com/yeshiqing/tampermonkey-scripts
 // @icon           https://upload-images.jianshu.io/upload_images/1231311-1d5a2ebbd33475e1.png
 // ==/UserScript==
 
 // Disable these keys when no keyboard modifier is pressed
-let std_keycodes = new Set([
-    // Add keycodes as desired
-    // 37, 38, 39, 40 // Arrow Keys.
+let std_keys = new Set([
+    // 'ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown'
 ])
 
 // Disable these keys when Meta key is pressed.
-let meta_keycodes = new Set([
-    // Add keycodes as desired
-    13 // Command + enter
+let meta_keys = new Set([
+    'Enter' // Command + enter
 ])
 
 // Disable these keys when Alt key is pressed.
-let alt_keycodes = new Set([
-    // Add keycodes as desired
-    // 83 // Alt + S
+let alt_keys = new Set([
+    // 's' // Alt + s
 ])
 
-
-let isMac = navigator.platform.indexOf('Mac') >= 0
+let platform = navigator.userAgentData?.platform || navigator.platform
+let isMac = platform.includes('macOS') || platform.includes('Mac')
 
 window.addEventListener('keydown', function (e) {
-    let keycode_set
+    let key
     if (isMac ? e.metaKey : e.ctrlKey) {
-        keycode_set = meta_keycodes
+        key = meta_keys
     } else if (e.altKey) {
-        keycode_set = alt_keycodes
+        key = alt_keys
     } else {
-        keycode_set = std_keycodes
+        key = std_keys
     }
 
-    if (keycode_set.has(e.keyCode)) {
+    if (key.has(e.key)) {
         e.stopImmediatePropagation()
     }
 }, true)
